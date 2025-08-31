64007eaab6e83c473897efc9af429f155dfe44cc
Functional Neuro Training <hokutomiyazaki312@gmail.com>
2025-08-31 23:11:56 +0900

    Create aw.js

---
+++ aw.js
@@ -0,0 +1,50 @@
+// Service Worker for OKN検査 PWA
+const CACHE_NAME = 'okn-test-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './FNT512.png',
+  './FNT512-transparent.png',
+  './manifest.json'
+];
+
+// インストールイベント
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// フェッチイベント
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュがあればそれを返す、なければネットワークから取得
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// アクティベートイベント（古いキャッシュの削除）
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
